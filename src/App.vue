25b59251c13fea3e376f1c6f777694a17426e47f
craftpaperbag <kazu11518@gmail.com>
2024-09-15 01:11:19 +0900

    tmp save(editing style.css)

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,20 @@
 import { ref } from 'vue'
 
 const message = ref('Hello Vue')
+
+const count = ref(0)
+
+function countup(){
+  count.value++
+}
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <button @click="countup">count is {{ count }}</button>
+
 </template>
 
 <style scoped>
+  
 </style>
